a6ed5da6cb653efa76eb8ce8e955e97994f8f790
izyvolo1-svg <izyvolo1@umbc.edu>
2026-04-27 17:07:03 +0000

    intput output

--- Project_2.s
+++ Project_2.s
@@ -1,6 +1,9 @@
 .section .data
-prompt: .ascii "enter text: "
+prompt: .ascii "Enter Number: "
 len = . - prompt
+
+output: .ascii "The double is: "
+output_len = . - prompt
 
 .section .bss
 .lcomm buffer, 128
@@ -9,24 +12,50 @@ len = . - prompt
 .globl _start
 
 _start:
+# prints input message
     mov $1, %rax
     mov $1, %rdi
     mov $prompt, %rsi
     mov $len, %rdx
     syscall
 
+# reads input from command line
     mov $0, %rax
     mov $0, %rdi
     mov $buffer, %rsi
     mov $128, %rdx
     syscall
 
-    mov %rax, %rdx
+# removes newline from input
+    cmp $0, %rax
+    je done_strip
+
+    dec %rax
+    
+    mov $buffer, %rsi
+    add %rax, %rsi
+
+    cmpb $10, (%rsi)
+    je done_strip
+
+    inc %rax
+
+    done_strip:
+# prints output text
     mov $1, %rax
     mov $1, %rdi
-    mov $buffer, %rsi
+    mov $output, %rsi
+    mov $output_len, %rdx
     syscall
 
+# prints user input
+    #mov %rax, %rdx
+    #mov $1, %rax
+    #mov $1, %rdi
+    #mov $buffer, %rsi
+    #syscall
+
+# exits
     mov $60, %rax
     xor %rdi, %rdi
     syscall
